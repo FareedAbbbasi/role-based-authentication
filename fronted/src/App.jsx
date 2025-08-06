8a4f90296067c4f1b09eef218bd5df528b5234ae
FareedAbbbasi <codingabbas0999@gmail.com>
2025-08-06 19:35:19 +0500

    Set up initial frontend and backend project structure

---
+++ fronted/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+   <div className='text-center'>
+    <h1 className='text-[40px] text-amber-200'>hello world</h1>
+   </div>
+  )
+}
+
+export default App
