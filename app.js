73f90c9357b6873f7864e1b6db27748e830901c8
FareedAbbbasi <codingabbas0999@gmail.com>
2025-08-07 20:33:51 +0500

    fix issue to the imports and missing the .js on the model in the import

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express, { json, urlencoded } from "express";
 const app = express();
 import cors from "cors";
+import AuthRoute from "./routes/auth.js"
 import ConnectDB from "./db/dbConnection.js";
 app.use(cors())
 app.use(json())
 
 app.use(urlencoded({ extended: true }))
 
+app.use("/api/auth", AuthRoute)
 
 
 app.listen(4000, () => {
